fa402b5d1d1c5f039f080de7a6b2071416c413a3
lksuraj <kayush1121@gmail.com>
2022-01-10 17:56:01 +0530

    this is my meme application.

--- app/src/main/java/com/example/memeshare/MainActivity.kt
+++ app/src/main/java/com/example/memeshare/MainActivity.kt
@@ -4,15 +4,12 @@ import android.content.Intent
 import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ImageView
 import android.widget.ProgressBar
 import android.widget.Toast
 import com.android.volley.Request
-import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
-import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.DataSource
@@ -22,14 +19,14 @@ import com.bumptech.glide.request.target.Target
 
 class MainActivity : AppCompatActivity() {
 
-    var currentMemeUrl: String? = null
+    private var currentMemeUrl: String? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         loadMeme()
     }
 
-    fun loadMeme(){
+    private fun loadMeme(){
         val imageP = findViewById<ImageView>(R.id.imageP)
         val progress = findViewById<ProgressBar>(R.id.progress)
         progress.visibility = View.VISIBLE
